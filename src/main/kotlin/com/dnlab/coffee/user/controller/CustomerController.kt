0a1ab8ae5ea6e68583b0f379b8cab37425639792
kang815595@gmail.com <Kang815595@gmail.com>
2023-10-12 21:21:17 +0900

    feat: 고객 회원가입 구현

--- src/main/kotlin/com/dnlab/coffee/user/controller/CustomerController.kt
+++ src/main/kotlin/com/dnlab/coffee/user/controller/CustomerController.kt
@@ -3,13 +3,26 @@ package com.dnlab.coffee.user.controller
 import com.dnlab.coffee.user.domain.Customer
 import com.dnlab.coffee.user.service.CustomerService
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
+@RequestMapping("/customer")
 class CustomerController(
     private val customerService: CustomerService
 ) {
-    fun addCustomer(customer: Customer): String {
+
+    @GetMapping("/new")
+    fun showCustomerForm(): String {
+        return "customer/new"
+    }
+
+    @PostMapping("/new")
+    fun addCustomer(customer: Customer,
+                    @RequestParam(required = false, defaultValue = "true") fromPayment: Boolean): String {
         customerService.addCustomer(customer)
-        TODO("view")
+        return if (fromPayment) "redirect:/order" else "redirect:/"
     }
 }
